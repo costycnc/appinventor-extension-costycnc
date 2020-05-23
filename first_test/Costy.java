1e6cfe5c054a92197b1803db7fce0113cac9d58d
Boboaca Costel <bobyca2003@yahoo.com>
2020-05-23 09:25:30 -0700

    Add files via upload

---
+++ first_test/Costy.java
@@ -0,0 +1,45 @@
+package Costy;
+// inspired from http://kio4.com/appinventor/125B_extensiones_crear_i.htm
+import android.hardware.usb.UsbDevice;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.DesignerProperty;
+import com.google.appinventor.components.annotations.PropertyCategory;
+import com.google.appinventor.components.annotations.SimpleEvent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.common.PropertyTypeConstants;
+import com.google.appinventor.components.runtime.util.MediaUtil;
+import com.google.appinventor.components.runtime.*;
+
+@DesignerComponent(version = Costy.VERSION,
+    description = "costy test https://www.costycnc.it",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "")
+@SimpleObject(external = true)
+public class Costy extends AndroidNonvisibleComponent implements Component {
+
+    public static final int VERSION = 1;
+    private ComponentContainer container;
+
+ 
+    public Costy(ComponentContainer container) {
+        super(container.$form());
+        this.container = container;
+
+    }
+	
+	    @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+    public double Cateto_A() {
+        return 100;
+    }
+	
+    @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+    public String test() {
+		String test = "test";
+		
+        return test;
+    }
+ } 
